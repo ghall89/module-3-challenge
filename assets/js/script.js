0e4db08cb1ac4ab212d42bd4bda37edbe166b1d7
Graham Hall <ghall89@me.com>
2021-02-10 17:29:10 -0500

    added password generation

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,19 +1,21 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+characters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "1", "2", "3", "4","5", "6", "7", "8", "9", "0", "?", "!", "*", "(", ")", "@", "#", "$", "&"];
+
 // Write password to the #password input
 function writePassword() {
 
-  var pswdLength = length();
-  console.log(pswdLength);
-  // var password = generatePassword();
-  // var passwordText = document.querySelector("#password");
+  var passLength = length();
 
-  // passwordText.value = password;
+  var password = generatePassword(passLength);
+  var passwordText = document.querySelector("#password");
 
+  passwordText.value = password;
 
 }
 
+// ask for password length and check to make sure it meets requirements
 var length = function() {
   var getLength = false;
   
@@ -27,9 +29,22 @@ var length = function() {
       getLength = false;
     }
   }
-
 }
 
+// generate a password with the number of characters defined in length
+var generatePassword = function(length) {
+
+  var generatedPass = "";
+  var character;
+
+  for (i = 0; i < length; i++) {
+    var random = Math.round(Math.random()*(characters.length-1) + 1);
+    console.log(random);
+    character = characters[random-1];
+    generatedPass = generatedPass + character;
+  }
+  console.log(generatedPass);
+}
 
 
 // Add event listener to generate button
